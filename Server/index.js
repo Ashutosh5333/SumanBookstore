28aed39db20e4216709b8b88576b0b8c0c6b8098
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-06-24 13:13:06 +0530

    Structure is done and  login and signup from backend

---
+++ Server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { connection } = require("./config/db");
+const cors = require("cors");
+const { UserRouter } = require("./routes/user.route");
+
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome home ");
+});
+
+
+app.use(UserRouter);
+
+
+app.listen(8000, async (req, res) => {
+  try {
+    await connection;
+    console.log("connected to database");
+  } catch (err) {
+    console.log("comething went wrong in connected");
+    console.log(err);
+  }
+  console.log("listening on port 8000");
+});
---
+++ Server/routes/user.route.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const { Usermodel } = require("../models/User.model");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken");
+
+const UserRouter = express.Router();
+
+
+ 
+ 
+    UserRouter.post("/signup",async (req,res) =>{
+       const {email,password ,name} = req.body
+
+        const useravailable = await Usermodel.findOne({email})
+       
+          if(useravailable){
+            res.send("user is already present")
+            return 
+          }
+          try{
+            bcrypt.hash(password, 4, async function(err, hash) {
+             
+              const  NewUser = new Usermodel({email,password:hash,name})
+              await NewUser.save()
+              res.send("Signup Sucessfully") 
+          });
+          
+          }
+          catch(err){
+            console.log(err)
+            res.send("Something went wrong")
+          }
+
+      })
+
+
+       UserRouter.post("/login", async (req,res) =>{
+          const {email,password,name} = req.body;
+
+           try{
+              const user = await Usermodel.find({email})
+           
+              if(user.length>0){
+                const hasedpassword = user[0].password
+            
+                const userdata = await Usermodel.find({email})
+                 bcrypt.compare(password, hasedpassword, function(err, result) {
+                    if(result){
+                       const token = jwt.sign({userId:user[0]._id},"hush")
+                      res.send({"msg":"login Sucessfully","token":token ,
+                       data:{
+                         userEmail:userdata[0].email,
+                         userName:userdata[0].name
+                       }
+                    })
+                    }
+                    else{
+                      res.send({ "msg":"Please check password"})
+                    }
+                });
+
+                }else{
+                  res.status(400).send({ "msg": "Email id not registered" });
+   
+                }
+           }
+           catch(err){
+              res.send({  "msg" :"Something went wrong"})
+           }
+       })
+
+  
+
+
+
+
+    module.exports={UserRouter}
