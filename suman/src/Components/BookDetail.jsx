1f41756a9a2639b000abf397bb13ce52e6b2f0cb
Ashutosh5333 <lakshakarashutosh@gmail.com>
2023-06-25 16:49:56 +0530

    updated single page

--- suman/src/Components/BookDetail.jsx
+++ suman/src/Components/BookDetail.jsx
@@ -6,14 +6,18 @@ import {
   Text,
   Flex,
   Button,
+  Heading,
   useToast,
+  useBreakpointValue,
 } from "@chakra-ui/react";
+
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { ADDCart, GetSinglebookdetail } from "../Redux/AppReducer/Action";
 import { Skelton } from "./Skelton";
 
 export const BookDetail = () => {
+  const SmallScreen = useBreakpointValue({ base: true, md: false, lg: false });
   const toast = useToast();
   const [loading, setLoading] = useState(false);
   const [single, Setsingle] = useState();
@@ -57,74 +61,60 @@ export const BookDetail = () => {
 
   return (
     <>
-      <Box
-        width={{ base: "90vw", md: "90vw", lg: "90vw" }}
-        m="auto"
-        mt="-20"
-        p="6"
-        mb="20"
-        spacing={3}
-      >
-        {single ? (
-          <Flex
-            boxShadow={"dark"}
-            justifyContent={"space-between"}
-            flexDirection={"column"}
-            gap="5"
-          >
-            <Flex w="60%" m="auto">
-              <Card m="auto">
-                <Image
-                  objectFit="cover"
-                  boxSize={{ base: "300", md: "400", lg: "500" }}
-                  m="auto"
-                  borderRadius="lg"
-                  src={single?.image}
-                />
-              </Card>
-            </Flex>
 
-            <Flex w="90%" m="auto" mt="10" flexDirection={"column"}>
-              <Box w="90%" m="auto">
-                <Text
-                  fontWeight={"600"}
-                  fontSize={{ base: "1.5rem", md: "1.5rem", lg: "2rem" }}
-                  color="#050452"
-                >
-                  {` Author -  ${single?.author} `}{" "}
-                </Text>
-              </Box>
+<Flex flexDirection={"column"} gap="10px">
 
-              <Box w="90%" m="auto" mt="10">
-                <Text fontWeight={"400"} color="#050452" letterSpacing={"1px"}>
-                  {single?.summary}{" "}
-                </Text>
-              </Box>
 
-              <Flex justifyContent={"space-between"} w="100%" m="auto" mt={{base:"10"}}>
-                <Box>
-                  <Text
-                    fontWeight={"600"}
-                    fontSize={{ base: "1.2rem", md: "1.5rem", lg: "2rem" }}
-                    color="#050452"
-                  >
-                    {single?.title}{" "}
-                  </Text>
-                </Box>
-                <Box>
-                  <Text
-                    fontWeight={"600"}
-                    fontSize={{ base: "1.2rem", md: "1.5rem", lg: "2rem" }}
-                    color="#050452"
-                  >
-                    {`Price - ${single?.price}`}{" "}
-                  </Text>
-                </Box>
-              </Flex>
+      
 
-              <Box w="90%" m="auto" mt="10">
-                <Button
-                  color="skyblue"
+{!SmallScreen && (
+  <Box
+    width={{ base: "80vw", md: "90vw", lg: "90vw" }}
+    m="auto"
+    mt="-20"
+    p="6"
+    mb="20"
+    spacing={3}
+  >
+    {single ? (
+      <Flex boxShadow={"dark"} justifyContent={"space-between"} gap="5">
+        <Flex  w="40%">
+          <Card m="auto">
+            <Image
+              boxSize="500px"
+              m="auto"
+              borderRadius="lg"
+              src={single?.image}
+            />
+          </Card>
+        </Flex>
+
+        <Flex
+          w="50%"
+          flexDirection={"column"}
+        >
+          <Box w="90%" m="auto">
+            <Text fontWeight={"600"} color="#050452">
+              {" "}
+               {` Price- ${single?.price}`}
+            </Text>
+          </Box>
+          <Box w="90%" m="auto" mt="5">
+            <Heading color="#050452"> {single?.author} </Heading>
+          </Box>
+
+          <Box w="90%" m="auto" mt="5">
+            <Text fontWeight={"600"} letterSpacing={"1px"}>
+              {single?.title}{" "}
+            </Text>
+          </Box>
+
+          <Box w="90%" m="auto" mt="5">
+            <Text fontWeight={"600"}>{single?.summary} </Text>
+          </Box>
+
+          <Box w="90%" m="auto" mt="5">
+            <Button  color="skyblue"
                   bg="black"
                   isLoading={loading}
                   onClick={() =>
@@ -135,18 +125,96 @@ export const BookDetail = () => {
                       single?.price,
                       single?.title
                     )
-                  }
-                >
-                  {" "}
-                  Add To Cart{" "}
-                </Button>
-              </Box>
-            </Flex>
-          </Flex>
-        ) : (
-          <Skelton />
-        )}
-      </Box>
-    </>
-  );
+                  } >  Add To Cart </Button>
+          </Box>
+        </Flex>
+      </Flex>
+    ) : (
+      <Skelton />
+    )}
+
+    
+  </Box>
+)}
+
+
+
+
+{SmallScreen && (
+  <Box
+    width={{ base: "95vw", md: "90vw" }}
+    m="auto"
+    p="6"
+    spacing={3}
+    mt="-10"
+  >
+    <Flex h="60vh" boxShadow={"dark"} flexDirection={"column"} gap="5">
+      <Flex w="90%" m="auto">
+        <Box m="auto">
+          <Image
+            boxSize="400px"
+            m="auto"
+            src={single?.image}
+          />
+        </Box>
+      </Flex>
+
+      <Flex h="60vh" w="90%" m="auto" flexDirection={"column"}>
+        <Box w="90%" m="auto" textAlign={"center"}>
+          <Text fontWeight={"600"} color="#050452">
+          {` Price- ${single?.price}`}
+          </Text>
+        </Box>
+        <Box w="90%" m="auto" mt="5">
+          <Heading textAlign={"center"} color="#050452">
+            {" "}
+            {single?.author}{" "}
+          </Heading>
+        </Box>
+
+        <Box w="90%" m="auto" mt="5">
+          <Text
+            textAlign={"center"}
+            fontWeight={"400"}
+            letterSpacing={"1px"}
+          >
+            {single?.summary}{" "}
+          </Text>
+        </Box>
+
+        <Box w="90%" m="auto" mt="5">
+          <Text textAlign={"center"} fontWeight={"600"}>
+            {single?.title}{" "}
+          </Text>
+        </Box>
+
+        <Box w="90%" m="auto" mt="5" textAlign={"center"}>
+        <Button  color="skyblue"
+                  bg="black"
+                  isLoading={loading}
+                  onClick={() =>
+                    handleAddTocart(
+                      single?.image,
+                      single?.author,
+                      single?.summary,
+                      single?.price,
+                      single?.title
+                    )
+                  } >  Add To Cart </Button>
+        </Box>
+      </Flex>
+    </Flex>
+
+   
+
+  </Box>
+)}
+
+
+
+</Flex>
+
+
+  </>
+  )
 };
